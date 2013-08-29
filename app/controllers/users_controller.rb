54281765e34ba05abece73dfc33191bbf0d20d9f
Mohit Singh <mohit.singh@weboniselab.com>
2013-08-29 15:22:00 +0530

    Finish user Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:sucess] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render new_user_path
+    end
+  end
+
+  def show
+    @user = User.find(1) #(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email) #, :password, :salt, :encrypted_password)
+  end
+
 end
